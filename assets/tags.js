4c73894f75d07a2c540510ee0c3d94ab6cde5bb7
webninjasi <webninjasi.0@gmail.com>
2022-09-29 00:51:03 +0300

    Update tags page

--- assets/tags.js
+++ assets/tags.js
@@ -3,42 +3,120 @@ var colors = ["BL", "BV", "CE", "CEP", "CH", "CS", "G", "J", "N", "N2", "PT", "P
 function refresh() {
   var sample = escapeHTML(document.getElementById('sample').value);
   var html = [
-    '<span class="TI">&lt;TI&gt;' + sample + '<span class="CL">&lt;CL&gt;' + sample + '</span></span>',
-    '<div class="TD">&lt;TD&gt;' + sample + '<div class="TG">&lt;TG&gt;' + sample + '</div></div>',
+    '<thead>',
+    '<tr><th>CODE</th><th>PREVIEW</th></tr>',
+    '</thead>',
+    '<tbody>',
+    row(
+      '&lt;TI&gt;' + sample + '&lt;CL&gt;' + sample,
+      '<span class="TI">' + sample + '<span class="CL">' + sample + '</span></span>'
+    ),
+    row(
+      '&lt;TD&gt;' + sample + '\\n&lt;TG&gt;' + sample,
+      '<div class="TD">' + sample + '<div class="TG">' + sample + '</div></div>'
+    ),
   ];
 
   for (var i = 0; i < colors.length; i ++) {
-    html.push('<span class="' + colors[i] + '">&lt;' + colors[i] + '&gt;' + sample + '</span>');
+    html.push(
+      row(
+        '&lt;' + colors[i] + '&gt;' + sample,
+        '<span class="' + colors[i] + '">' + sample + '</span>'
+      )
+    );
   }
 
   html.push.apply(html, [
-    '<a href="#">&lt;a href="event:eventName for eventTextAreaCallback"&gt;' + sample + '&lt;/a&gt;</a>',
-    '<b>&lt;b&gt;' + sample + '&lt;/b&gt;</b>',
-    '<i>&lt;i&gt;' + sample + '&lt;/i&gt;</i>',
-    '<u>&lt;u&gt;' + sample + '&lt;/u&gt;</u>',
-    '<span style="font-family:Arial;">&lt;font face="Arial"&gt;' + sample + '&lt;/font&gt;</span>',
-    '<span style="color:#ff0000;">&lt;font color="#ff0000"&gt;' + sample + '&lt;/font&gt;</span>',
-    '<span style="font-size:20px;">&lt;font size="20"&gt;' + sample + '&lt;/font&gt;</span>',
-    sample.substr(0, sample.length / 2) + '\\n<br />' + sample.substr(sample.length / 2),
-    sample.substr(0, sample.length / 2) + '\\t\t' + sample.substr(sample.length / 2),
-    '<li>&lt;li&gt;' + sample + '&lt;/li&gt;</li>',
-    '<p style="text-align:center;">&lt;p align="center"&gt;' + sample + '</p>',
-    '<p style="text-align:right;">&lt;p align="right"&gt;' + sample + '</p>',
-    '<p style="text-align:justify;">&lt;p align="justify"&gt;' + sample + '</p>',
-    '<p style="text-align:left;">&lt;p align="left"&gt;' + sample + '</p>',
-    '<span style="margin-left:10px;">&lt;textformat leftmargin="10"&gt;' + sample + '&lt;/textformat&gt;</span>',
+    row(
+      '&lt;a href="event:eventName for eventTextAreaCallback"&gt;' + sample + '&lt;/a&gt;',
+      '<a href="#">' + sample + '</a>'
+    ),
+    row(
+      '&lt;b&gt;' + sample + '&lt;/b&gt;',
+      '<b>' + sample + '</b>'
+    ),
+    row(
+      '&lt;i&gt;' + sample + '&lt;/i&gt;',
+      '<i>' + sample + '</i>'
+    ),
+    row(
+      '&lt;u&gt;' + sample + '&lt;/u&gt;',
+      '<u>' + sample + '</u>'
+    ),
+    row(
+      '&lt;font face="Arial"&gt;' + sample + '&lt;/font&gt;',
+      '<span style="font-family:Arial;">' + sample + '</span>'
+    ),
+    row(
+      '&lt;font color="#ff0000"&gt;' + sample + '&lt;/font&gt;',
+      '<span style="color:#ff0000;">' + sample + '</span>'
+    ),
+    row(
+      '&lt;font size="20"&gt;' + sample + '&lt;/font&gt;',
+      '<span style="font-size:20px;">' + sample + '</span>'
+    ),
+    row(
+      sample + '\\n' + sample,
+      sample + '<br />' + sample
+    ),
+    row(
+      sample + '\\t' + sample,
+      sample + '\t' + sample
+    ),
+    row(
+      '&lt;li&gt;' + sample + '&lt;/li&gt;',
+      '<li>' + sample + '</li>'
+    ),
+    row(
+      '&lt;p align="center"&gt;' + sample,
+      '<p style="text-align:center;">' + sample + '</p>'
+    ),
+    row(
+      '&lt;p align="right"&gt;' + sample,
+      '<p style="text-align:right;">' + sample + '</p>'
+    ),
+    row(
+      '&lt;p align="justify"&gt;' + sample,
+      '<p style="text-align:justify;">' + sample + '</p>'
+    ),
+    row(
+      '&lt;p align="left"&gt;' + sample,
+      '<p style="text-align:left;">' + sample + '</p>'
+    ),
+    row(
+      '&lt;textformat leftmargin="10"&gt;' + sample + '&lt;/textformat&gt;',
+      '<span style="margin-left:10px;">' + sample + '</span>'
+    ),
 
-    '<div style="line-height:0.6;">&lt;textformat leading="-8"&gt;' + sample.substr(0, sample.length / 2)
-    + '\\n<br />' + sample.substr(sample.length / 2) + '&lt;/textformat&gt;</div>',
-    
-    '<div style="min-width:500px;display:inline-block;"><div style="min-width:250px;display:inline-block;">' +
-    '&lt;textformat tabstops="[250,500]"&gt;<br />' + sample.substr(0, sample.length / 2)
-    + '\\t</div>' + sample.substring(sample.length / 2, sample.length * 3 / 4) + '\\t</div>' + sample.substr(sample.length * 3 / 4) +
-    '&lt;/textformat&gt;',
-    ''
+    row(
+      '&lt;textformat leading="-8"&gt;' +
+      sample + '\\n' + sample +
+      '&lt;/textformat&gt;',
+      '<div style="line-height:0.6;">' +
+      sample + '<br />' + sample +
+      '</div>'
+    ),
+
+    row(
+      '&lt;textformat tabstops="[250,500]"&gt;' +
+      sample +'\\t' + sample + '\\t' + sample +
+      '&lt;/textformat&gt;',
+      '<div style="min-width:500px;display:inline-block;">' +
+      '<div style="min-width:250px;display:inline-block;">' +
+      '<br />' +
+      sample + '</div>' +
+      sample + '</div>' +
+      sample
+    ),
+  
+    '</tbody>'
   ]);
 
-  document.getElementById('content').innerHTML = html.join('<br /><br />');
+  document.getElementById('tags-content').innerHTML = html.join('');
+}
+
+function row(left, right) {
+  return '<tr>' + '<td>' + left + '</td>' + '<td>' + right + '</td>' + '</tr>';
 }
 
 document.getElementById('sample').addEventListener('keyup', function() {
