469b3bcc4fd03ced147387136a0f65db8b54baf0
webninjasi <webninjasi.0@gmail.com>
2022-11-18 13:20:20 +0300

    Add private collections to image browser

--- assets/images.js
+++ assets/images.js
@@ -2,6 +2,8 @@
   const id = selector => document.getElementById(selector);
   const toggle = (elmId, value) => id(elmId).style.display = (value == null && id(elmId).style.display == "none" || value) ? 'block' : "none";
   const defaultContent = id('image-wrapper').innerHTML;
+  let localImages = {};
+  let currentOverlayImgId;
 
   window.toggle = toggle;
   window.load = (btn, elmId, image) => {
@@ -9,6 +11,44 @@
     id(elmId).src = `http://images.atelier801.com/${image}`;
     id(elmId).style.display = null;
   };
+
+  window.addPrivate = () => {
+    const newImages = JSON.parse('{' + id('add_image_output').value + '}');
+    let count = 0;
+
+    for (let id in newImages) {
+      if (!allImages[id]) {
+        count ++;
+      }
+
+      localImages[id] = newImages[id];
+      allImages[id] = newImages[id];
+    }
+
+    save();
+    updateContent();
+
+    id('add_image_result').innerHTML = `Added ${count} new images!`;
+  }
+
+  id('image_overlay_delete').addEventListener('click', function() {
+    if (currentOverlayImgId == null) {
+      return;
+    }
+
+    if (!localImages[currentOverlayImgId]) {
+      return;
+    }
+
+    delete localImages[currentOverlayImgId];
+    delete allImages[currentOverlayImgId];
+
+    toggle('image_overlay', false);
+    save();
+    updateContent();
+  });
+
+  load();
 
   // Init
   let filterText = '';
@@ -29,35 +69,17 @@
     document.execCommand('copy');
   }
 
-  const imageElms = [...document.getElementsByClassName('image-img')];
-  imageElms.map(
-    elm => {
-      elm.addEventListener('load', function() {
-        if (this.nextElementSibling.className == "image-dimensions") {
-          this.nextElementSibling.innerHTML = `${this.naturalWidth}x${this.naturalHeight}`;
-        }
-      });
-
-      elm.addEventListener('click', function() {
-        id('image_overlay_img').src = this.src;
-        id('image_overlay').style.display = 'block';
-        id('image_overlay_id').value = this.dataset.code;
-        id('image_overlay_code').value = `tfm.exec.addImage("${this.dataset.code}", "!1", 0, 30, nil, 1, 1, 0, 1, 0, 0, false)`;
-        id('image_overlay_code2').value = `{"${this.dataset.code}", ${this.naturalWidth}, ${this.naturalHeight}},`;
-      });
-    }
-  );
-
   id('add_image_input').addEventListener('change', function() {
     id('add_image_output').value = [
         ...this.value.replace(/https?\S+/g, '').matchAll(
-          /^(?:(\s*[a-z0-9]+\.[a-z]{3})? +?)?(.+?)$/mig
+          /^(\s*[a-z0-9]{11}\.(?:png|jpg|jpeg|gif))?(.+?)?$/mig
         )
       ]
       .map(m => ({
         "image": m[1] && m[1].trim(),
-        "tags": m[2].replace(/[^a-z0-9,\s]+/ig, '').trim().toLowerCase()
+        "tags": m[2] ? m[2].replace(/[^a-z0-9,\s]+/ig, '').trim().toLowerCase() : null
       }))
+      .filter(x => x.tags || x.image)
       .reduce(
         (ret, line) => {
           if (!line.image) {
@@ -65,13 +87,21 @@
             return ret;
           }
 
-          ret.push(`"${line.image}": "${ret.__tags ? ret.__tags + ", " : ""}${line.tags}",`)
+          if (line.tags) {
+            if (ret.__tags) {
+              line.tags = ret.__tags + ', ' + line.tags
+            }
+          } else {
+            line.tags = ret.__tags
+          }
+
+          ret.push(`"${line.image}": "${line.tags || ''}"`)
           
           return ret;
         },
         []
       )
-      .join('\n');
+      .join(',\n');
   });
 
   // Functions
@@ -116,17 +146,70 @@
       <i>${allImages[image]}</i>
       <br />
       <br />
-      <img class="image-img" src="http://images.atelier801.com/${image}" data-code="${image}" loading="lazy" />
+      <img class="image-img" src="http://images.atelier801.com/${image}" data-code="${image}" data-islocal="${Boolean(localImages[image])}" loading="lazy" />
       <p class="image-dimensions"></p>
     </div>
   </div>
 </div>
     `).join('');
+
+    const imageElms = [...document.getElementsByClassName('image-img')];
+    imageElms.map(
+      elm => {
+        elm.addEventListener('load', function() {
+          if (this.nextElementSibling.className == "image-dimensions") {
+            this.nextElementSibling.innerHTML = `${this.naturalWidth}x${this.naturalHeight}`;
+          }
+        });
+  
+        elm.addEventListener('click', function() {
+          currentOverlayImgId = this.dataset.code;
+          id('image_overlay_delete').style.display = this.dataset.islocal == 'false' ? 'none' : null;
+          id('image_overlay_img').src = this.src;
+          id('image_overlay').style.display = 'block';
+          id('image_overlay_id').value = this.dataset.code;
+          id('image_overlay_code').value = `tfm.exec.addImage("${this.dataset.code}", "!1", 0, 30, nil, 1, 1, 0, 1, 0, 0, false)`;
+          id('image_overlay_code2').value = `{"${this.dataset.code}", ${this.naturalWidth}, ${this.naturalHeight}},`;
+        });
+      }
+    );
+
+    filterContent();
   }
 
   function errorSet(err) {
     id('image-wrapper').innerHTML = defaultContent;
     id('error').innerHTML = err.stack;
     console.error(err.stack);
-  };
+  }
+
+  function load() {
+    if (!window.localStorage) {
+      return;
+    }
+
+    const privateCollection = JSON.parse(localStorage.getItem('privateCollection') || '{}');
+
+    if (!Object.keys(privateCollection).length) {
+      return;
+    }
+
+    for (let id in privateCollection) {
+      allImages[id] = privateCollection[id];
+      localImages[id] = privateCollection[id];
+    }
+  }
+
+  function save() {
+    if (!window.localStorage) {
+      return;
+    }
+
+    if (!Object.keys(localImages).length) {
+      localStorage.removeItem('privateCollection');
+      return;
+    }
+
+    localStorage.setItem('privateCollection', JSON.stringify(localImages));
+  }
 })();
